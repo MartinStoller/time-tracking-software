1f3970d29418b53dafdd94375183aca1b9b85aa8
MartinStoller <martin.stoller2@gmx.de>
2021-12-22 14:39:14 +0100

    implementing testdata to projects db

--- src/main/java/de/example/haegertime/projects/Project.java
+++ src/main/java/de/example/haegertime/projects/Project.java
@@ -1,24 +1,52 @@
 package de.example.haegertime.projects;
 
 import lombok.Data;
+import org.apache.tomcat.jni.Local;
+import org.sonatype.inject.Nullable;
+import org.springframework.validation.annotation.Validated;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Data
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name="projects") // this allows the mapping to our specific table in the db
+@Validated
 public class Project {
     @Id
+    @SequenceGenerator(
+            name="project_sequence",
+            sequenceName = "project_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "project_sequence"
+    )
     private Long id;
+    @NotNull @NotBlank
     private String title;
+    @NotNull
+    private LocalDate start;
+    @Nullable
+    private LocalDate end;
+    @NotNull @NotBlank
+    private String customer;
+    @NotNull @NotBlank
+    private String billingAddress;
+
 
     public Project(){} //Empty Constructor needed for hibernate
 
-    public Project(Long id, String title){
-        this.id = id;
+    public Project(String title, LocalDate start, LocalDate end, String customer, String billingAddress){
         this.title = title;
+        this.start = start;
+        this.end = end;
+        this.customer = customer;
+        this.billingAddress = billingAddress;
     }
     //TODO: implement attributes (getter/setter/equals will be handled by annotation)
 }
--- src/main/java/de/example/haegertime/projects/ProjectsConfig.java
+++ src/main/java/de/example/haegertime/projects/ProjectsConfig.java
@@ -4,15 +4,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class ProjectsConfig {
     @Bean
     CommandLineRunner clr_projects(ProjectRepository repository) {
         return args -> {
-            Project proj1 = new Project(111L, "title 1");
-            Project proj2 = new Project(999L, "la li li  lu");
+            Project proj1 = new Project("title 1", LocalDate.of(1995, Month.JULY, 22), LocalDate.of(2021, Month.AUGUST, 2), "BMW", "bmwler Straße 22");
+            Project proj2 = new Project("blabla", LocalDate.of(1992, Month.JULY, 13), null, "VW", "Vwler Straße 21");;
             repository.saveAll(List.of(proj1, proj2));
         };
     }
