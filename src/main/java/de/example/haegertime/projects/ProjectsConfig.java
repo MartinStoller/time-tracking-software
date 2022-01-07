8dd4ccec17d5189ec313b75d8c62fa47642a1cbb
MartinStoller <martin.stoller2@gmx.de>
2022-01-07 10:56:39 +0100

    playing around after merge

--- src/main/java/de/example/haegertime/projects/ProjectsConfig.java
+++ src/main/java/de/example/haegertime/projects/ProjectsConfig.java
@@ -11,18 +11,18 @@ import java.util.List;
 @Configuration
 public class ProjectsConfig {
 
-/*    @Bean
+    @Bean
     CommandLineRunner clrProjects(ProjectRepository repository) {
         return args -> {
-            Project proj1 = new Project("Homepage Maintenance BMW", LocalDate.of(1995, Month.JULY, 22), LocalDate.of(2021, Month.AUGUST, 2), "BMW AG", "80375 München, BlaBlaStraße 4");
-            Project proj2 = new Project("WebAppXY Extension SAP", LocalDate.of(1992, Month.JULY, 13), null, "VW", "84034 Landshut, Franz-Marc-Str. 18");
-            Project proj3 = new Project("Building the next Google", LocalDate.of(2005, Month.JULY, 23), null, "Startup3000", "50823 Köln, Liebigstraße 155");
-            Project proj4 = new Project("Cloud Computing project 500", LocalDate.of(2020, Month.JANUARY, 3), LocalDate.of(2021, Month.DECEMBER, 1), "Deutsche Bahn", "12053 Berlin, Berlinerstraße 12");
-            Project proj5 = new Project("Mission: Building the next Google", LocalDate.of(1992, Month.JULY, 13), LocalDate.of(2009, Month.JUNE, 12), "Nick&Marty AG", "19544 Schleswig, Nickelpeterstraße 2");
-            Project proj6 = new Project("Top Secret Spy Stuff", LocalDate.of(1992, Month.JULY, 13), null, "Baidu", "China, 59422145 Wuh Han, Chinesenweg 3");
-            Project proj7 = new Project("Bohrmaschinenautomatisierungsprojekt XY", LocalDate.of(1992, Month.JULY, 13), null, "Hilti", "Österreich, 20533 Bludenz, Am Ösieck 122");
+            Project proj1 = new Project("Homepage Maintenance BMW", LocalDate.of(1995, Month.JULY, 22), LocalDate.of(2021, Month.AUGUST, 2));
+            Project proj2 = new Project("WebAppXY Extension SAP", LocalDate.of(1992, Month.JULY, 13), null);
+            Project proj3 = new Project("Building the next Google", LocalDate.of(2005, Month.JULY, 23), null);
+            Project proj4 = new Project("Cloud Computing project 500", LocalDate.of(2020, Month.JANUARY, 3), LocalDate.of(2021, Month.DECEMBER, 1));
+            Project proj5 = new Project("Mission: Building the next Google", LocalDate.of(1992, Month.JULY, 13), LocalDate.of(2009, Month.JUNE, 12));
+            Project proj6 = new Project("Top Secret Spy Stuff", LocalDate.of(1992, Month.JULY, 13), null);
+            Project proj7 = new Project("Bohrmaschinenautomatisierungsprojekt XY", LocalDate.of(1992, Month.JULY, 13), null);
             repository.saveAll(List.of(proj1, proj2, proj3, proj4, proj5, proj6, proj7));
         };
 
-    }*/
+    }
 }
--- src/main/java/de/example/haegertime/users/UserConfig.java
+++ src/main/java/de/example/haegertime/users/UserConfig.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Configuration
 public class UserConfig {
 
-/*    @Bean
+    @Bean
     CommandLineRunner clrUsers(UserRepository userRepository){
         return  args -> {
             User usr1 = new User("Anton", "Aus Tirol", "1234567", "anton.austirol@gmx.de", Role.EMPLOYEE);
@@ -21,5 +21,5 @@ public class UserConfig {
             //User usr7 = new User("Waldo", "Holzkopf", "redbull!", "holzkopf@gmx.net", Role.EMPLOYEE);
             userRepository.saveAll(List.of(usr1, usr2, usr3));
         };
-    }*/
+    }
 }
